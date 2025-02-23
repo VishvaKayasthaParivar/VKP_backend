46d9689b4be845758a7186f162ad25e7b74cc955
ab1123 <asarma626@gmail.com>
2025-02-24 03:49:48 +0530

    Done

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
+const cors=require('cors')
 
 const app = express();
+
+app.use(cors({
+    origin: "*",
+}))
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
